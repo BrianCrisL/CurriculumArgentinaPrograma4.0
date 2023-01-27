6d785d4a48d486d98fe27125351a06c49e397c69
BrianCrisL <brianleon22020@gmail.com>
2023-01-27 06:30:58 -0300

    CV Argentina Programa

---
+++ Curriculum.js
@@ -0,0 +1,24 @@
+document.getElementById('rojo').onclick=function(){
+    var element=document.getElementById('Izquierda');
+    element.style.backgroundColor='#900';
+    var element=document.getElementById('Centro');
+    element.style.backgroundColor='#fe5555';
+}
+document.getElementById('azul').onclick=function(){
+    var element=document.getElementById('Izquierda');
+    element.style.backgroundColor='#55abc7';
+    var element=document.getElementById('Centro');
+    element.style.backgroundColor='#BCE8F9';
+}
+document.getElementById('verde').onclick=function(){
+    var element=document.getElementById('Izquierda');
+    element.style.backgroundColor='#065300';
+    var element=document.getElementById('Centro');
+    element.style.backgroundColor='#65b75f';
+}
+document.getElementById('rosado').onclick=function(){
+    var element=document.getElementById('Izquierda');
+    element.style.backgroundColor='#a30872';
+    var element=document.getElementById('Centro');
+    element.style.backgroundColor='#e274bf';
+}
